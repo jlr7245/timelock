9208375cde009ee0f48b89eda8b75ee5acf5c6cc
J Silverstein <jess.pebble@gmail.com>
2019-07-23 00:51:47 -0400

    styling, add some constants

--- shared/constants.js
+++ shared/constants.js
@@ -1,5 +1,11 @@
+// config communications
 const CONFIG = 'config';
 const GET_CONFIG = 'getconfig';
 const UPDATE_CONFIG = 'updateconfig';
 const RESET_CONFIG = 'resetconfig';
 const MULTIPLIER = 5;
+
+// state update keys
+const SET_CONFIG = 'setconfig';
+const ADD_SITE = 'addsite';
+const UPDATE_SITE = 'updatesite';
