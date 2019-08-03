dfa4b17dbe4581831fe7a3b644552798d7d714dd
J Silverstein <jess.pebble@gmail.com>
2019-08-02 21:29:09 -0400

    parcel works!

---
+++ onboard/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.styles.css';
+
+const App = () => (
+  <div className="container">
+    <h1 className="title">timelock</h1>
+    <h3 className="subtitle">
+      A friendly warlock to help you with your time management by locking away
+      various sites.
+    </h3>
+  </div>
+);
+
+export default App;
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Timelock
 
 A friendly warlock that helps you manage your time!
+
+![GitHub release](https://img.shields.io/github/release/jlr7245/timelock?style=for-the-badge)  ![GitHub issues](https://img.shields.io/github/issues/jlr7245/timelock?style=for-the-badge)  
