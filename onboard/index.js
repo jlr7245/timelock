61c3eb925be593587a683cbc0b8303657a785637
J Silverstein <jess.pebble@gmail.com>
2019-09-22 21:51:14 -0400

    rm some bad imports

--- onboard/index.js
+++ onboard/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import '../assets/reset.css';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
